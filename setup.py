bcc32da7947818483611c02f10d5e8a9d5b472a4
Tianyu Du <tianyudu@stanford.edu>
2022-06-08 16:30:38 -0700

    update setup.py to fix installation from pip

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import pathlib
+import setuptools
 from setuptools import setup
 
 # The directory containing this file
@@ -10,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="torch_choice",
-    version="0.0.2.alpha",
+    version="0.0.3.alpha",
     description="A Pytorch Backend Library for Choice Modelling",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,7 +24,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["torch_choice"],
+    # packages=["torch_choice"],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     # install_requires=["torch"],
     # entry_points={
