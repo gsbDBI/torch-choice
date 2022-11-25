9641a6eeceb70b5a512337a06e07da2fcf2dc1cf
Tianyu Du <tianyudu@stanford.edu>
2022-11-25 02:23:06 -0800

    enhance run script

--- torch_choice/utils/run_helper.py
+++ torch_choice/utils/run_helper.py
@@ -14,7 +14,7 @@ from torch_choice.model.conditional_logit_model import ConditionalLogitModel
 from torch_choice.model.nested_logit_model import NestedLogitModel
 
 
-def run(model, dataset, dataset_test=None, batch_size=-1, learning_rate=0.01, num_epochs=5000, report_frequency=None):
+def run(model, dataset, dataset_test=None, batch_size=-1, learning_rate=0.01, num_epochs=5000, report_frequency=None, compute_std=True, return_final_training_log_likelihood=False):
     """All in one script for the model training and result presentation."""
     if report_frequency is None:
         report_frequency = (num_epochs // 10)
@@ -56,42 +56,51 @@ def run(model, dataset, dataset_test=None, batch_size=-1, learning_rate=0.01, nu
         test_ll = - model.negative_log_likelihood(dataset_test, dataset_test.item_index).detach().item()
         print('Test set log-likelihood: ', test_ll)
 
-    # current methods of computing standard deviation will corrupt the model, load weights into another model for returning.
-    state_dict = deepcopy(model.state_dict())
-    trained_model.load_state_dict(state_dict)
+    if not compute_std:
+        if return_final_training_log_likelihood:
+            return model, ll
+        else:
+            return model
+    else:
+        # current methods of computing standard deviation will corrupt the model, load weights into another model for returning.
+        state_dict = deepcopy(model.state_dict())
+        trained_model.load_state_dict(state_dict)
 
-    # get mean of estimation.
-    mean_dict = dict()
-    for k, v in model.named_parameters():
-        mean_dict[k] = v.clone()
+        # get mean of estimation.
+        mean_dict = dict()
+        for k, v in model.named_parameters():
+            mean_dict[k] = v.clone()
 
-    # estimate the standard error of the model.
-    if isinstance(model, ConditionalLogitModel):
-        def nll_loss(model):
-            y_pred = model(dataset)
-            return F.cross_entropy(y_pred, dataset.item_index, reduction='sum')
-    elif isinstance(model, NestedLogitModel):
-        def nll_loss(model):
-            d = dataset[torch.arange(len(dataset))]
-            return model.negative_log_likelihood(d, d['item'].item_index)
+        # estimate the standard error of the model.
+        if isinstance(model, ConditionalLogitModel):
+            def nll_loss(model):
+                y_pred = model(dataset)
+                return F.cross_entropy(y_pred, dataset.item_index, reduction='sum')
+        elif isinstance(model, NestedLogitModel):
+            def nll_loss(model):
+                d = dataset[torch.arange(len(dataset))]
+                return model.negative_log_likelihood(d, d['item'].item_index)
 
-    std_dict = parameter_std(model, nll_loss)
+        std_dict = parameter_std(model, nll_loss)
 
-    print('=' * 20, 'model results', '=' * 20)
-    report = list()
-    for coef_name, std in std_dict.items():
-        std = std.cpu().detach().numpy()
-        mean = mean_dict[coef_name].cpu().detach().numpy()
-        coef_name = coef_name.replace('coef_dict.', '').replace('.coef', '')
-        for i in range(mean.size):
-            report.append({'Coefficient': coef_name + f'_{i}',
-                           'Estimation': float(mean[i]),
-                           'Std. Err.': float(std[i])})
-    report = pd.DataFrame(report).set_index('Coefficient')
-    print(f'Training Epochs: {num_epochs}\n')
-    print(f'Learning Rate: {learning_rate}\n')
-    print(f'Batch Size: {batch_size if batch_size != -1 else len(dataset)} out of {len(dataset)} observations in total\n')
-    print(f'Final Log-likelihood: {ll}\n')
-    print('Coefficients:\n')
-    print(report.to_markdown())
-    return trained_model
+        print('=' * 20, 'model results', '=' * 20)
+        report = list()
+        for coef_name, std in std_dict.items():
+            std = std.cpu().detach().numpy()
+            mean = mean_dict[coef_name].cpu().detach().numpy()
+            coef_name = coef_name.replace('coef_dict.', '').replace('.coef', '')
+            for i in range(mean.size):
+                report.append({'Coefficient': coef_name + f'_{i}',
+                            'Estimation': float(mean[i]),
+                            'Std. Err.': float(std[i])})
+        report = pd.DataFrame(report).set_index('Coefficient')
+        print(f'Training Epochs: {num_epochs}\n')
+        print(f'Learning Rate: {learning_rate}\n')
+        print(f'Batch Size: {batch_size if batch_size != -1 else len(dataset)} out of {len(dataset)} observations in total\n')
+        print(f'Final Log-likelihood: {ll}\n')
+        print('Coefficients:\n')
+        print(report.to_markdown())
+        if return_final_training_log_likelihood:
+            return trained_model, ll
+        else:
+            return trained_model
