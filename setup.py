e8fb0a54ec8218f2830d13787b1a78831af9f1b6
Tianyu Du <tianyudu@stanford.edu>
2023-04-08 13:04:31 -0700

    fix version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="torch_choice",
-    version="0.0.6",
+    version="1.0.0",
     description="A Pytorch Backend Library for Choice Modelling",
     long_description=README,
     long_description_content_type="text/markdown",
