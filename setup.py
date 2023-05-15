f43f55fda458c252fb465b26e5cf3ab977752578
Tianyu Du <tianyudu@stanford.edu>
2023-05-15 10:03:57 -0700

    update version 1.0.1 --> 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="torch_choice",
-    version="1.0.1",
+    version="1.0.2",
     description="A Pytorch Backend Library for Choice Modelling",
     long_description=README,
     long_description_content_type="text/markdown",
