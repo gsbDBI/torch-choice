7d1d2f5e1094c5614ab971a194d231a424ebc38f
Tianyu Du <tianyudu@stanford.edu>
2025-03-03 17:08:34 -0800

    reduce to a smaller number of items
    https://github.com/gsbDBI/torch-choice-dev/issues/11

--- paper_performance_benchmarks/run_mlogit_num_items.R
+++ paper_performance_benchmarks/run_mlogit_num_items.R
@@ -22,7 +22,8 @@ final_likelihood.list <- c()
 for (seed in 1:num.seeds) {
   tic()
   print(paste0('Seed=', seed))
-  for (num_items in c(10, 30, 50, 100, 150, 200)) {
+  # for (num_items in c(10, 30, 50, 100, 150, 200)) {
+  for (num_items in c(10, 30, 50)) {
     # Read in the simulated dataset.
     df <- read.csv(str_glue("simulated_choice_data_num_items_experiment_{num_items}_items_seed_42.csv"))
     df$item_id <- as.factor(df$item_id)
